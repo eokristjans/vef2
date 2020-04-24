28c16fca6576159cce536820cb62cd35f7e9d3cb
erling <eok4@hi.is>
2020-04-24 12:10:22 +0000

    added comments

--- h1/api/users.js
+++ h1/api/users.js
@@ -13,6 +13,12 @@ const { query, pagedQuery } = require('../utils/db');
 const { isBoolean } = require('../utils/validation');
 const addPageMetadata = require('../utils/addPageMetadata');
 
+/**
+ * Returns a paged JSON object with up to 10 users.
+ *
+ * @param {Object} req may contain .query with page offset and limit
+ * @param {Object} res
+ */
 async function readUsersRoute(req, res) {
   const { offset = 0, limit = 10 } = req.query;
 
@@ -35,6 +41,13 @@ async function readUsersRoute(req, res) {
   return res.json(usersWithPage);
 }
 
+
+/**
+ * Returns a JSON object representing a user with id from req.params.
+ *
+ * @param {Object} req must contain user id in .params
+ * @param {Object} res
+ */
 async function readUserRoute(req, res) {
   const { id } = req.params;
 
@@ -47,17 +60,27 @@ async function readUserRoute(req, res) {
   return res.json(user);
 }
 
+
+/**
+ * Updates the user with id from req.params, changing
+ * the user's admin field to be that from req.body.
+ *
+ * @param {Object} req must contain user id in .params
+ * @param {Object} res
+ */
 async function updateUserRoute(req, res) {
   const { id } = req.params;
   const { user: currentUser } = req;
   const { admin } = req.body;
 
+  // Find the user being updated
   const user = await findById(id);
 
   if (!user) {
     return res.status(404).json({ error: 'User not found' });
   }
 
+  // Validate admin input
   if (!isBoolean(admin)) {
     return res.status(400).json({
       errors: [{
@@ -67,12 +90,14 @@ async function updateUserRoute(req, res) {
     });
   }
 
+  // Check if user is an admin trying to revoke his own admin privileges
   if (!admin && (currentUser.id === Number(id))) {
     return res.status(400).json({
       error: 'Can not remove admin privileges from self',
     });
   }
 
+  // Prepare query to update user
   const q = `
     UPDATE
       users
@@ -85,9 +110,16 @@ async function updateUserRoute(req, res) {
   return res.status(201).json(result.rows[0]);
 }
 
+/**
+ * Returns a JSON object representing the user that is stored in req.user.
+ *
+ * @param {Object} req must contain .user
+ * @param {Object} res
+ */
 async function currentUserRoute(req, res) {
   const { user: { id } = {} } = req;
 
+  // Find the current user and return entity if found
   const user = await findById(id);
 
   if (!user) {
@@ -97,23 +129,36 @@ async function currentUserRoute(req, res) {
   return res.json(user);
 }
 
+
+/**
+ * Updates the user that is stored in req.user, changing the
+ * values of the fields given in req.body.
+ *
+ * @param {Object} req must contain .user and may contain .body
+ * which may contain password or email to be updated.
+ * @param {Object} res
+ */
 async function updateCurrentUser(req, res) {
   const { id } = req.user;
 
+  // Find the current user
   const user = await findById(id);
 
   if (!user) {
     return res.status(404).json({ error: 'User not found' });
   }
 
+  // Get the password and email
   const { password, email } = req.body;
 
+  // Validate the input
   const validationMessage = await validateUser({ password, email }, true, id);
 
   if (validationMessage.length > 0) {
     return res.status(400).json({ errors: validationMessage });
   }
 
+  // Update user
   const result = await updateUser(id, password, email);
 
   if (!result) {
